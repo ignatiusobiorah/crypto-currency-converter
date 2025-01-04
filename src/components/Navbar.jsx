9cede7c1595a183f3f5974087d6820870634429e
ignatiusobiorah <iobiorah23@gmail.com>
2025-01-04 23:53:44 +0100

    Setup dev. env.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+   <nav className='bg-orange-600 text-black fixed w-full'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+            <h1>goat</h1>
+        </div>
+    </div>
+   </nav>
+  )
+}
+
+export default Navbar;
